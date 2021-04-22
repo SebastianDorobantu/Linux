5374bb370d71130ecbfc6ee949149ef102439335
SebastianDorobantu <Sebastian.dorobantul@gmail.com>
2021-04-22 16:41:59 +0300

    Gat

--- Env.sh
+++ Env.sh
@@ -16,6 +16,10 @@ confu=/etc/apache2/apache2.conf
 confc=/etc/httpd/conf/httpd.conf
 os=$(cat /etc/*release |grep 'ubuntu')
 arh=/var/Seb
+Updatevar="\e\n \e[1m\e[31mSTARTING \e[1mSYSTEM \e[1mUPDATE\e[0m \e\n" 
+Envar="\e\n \e[31m\e[1mINITIALIZING \e[1mENVIROMENT\e[0m \e\n"
+Compvar="\e\n \e[31m\e[1mENVIROMENT \e[1mINITIALIZED\e[0m \e\n"
+Restvar="\e\n \e[31m\e[1mENVIROMENT \e[1mRESTORE\e[0m \e\n"
 
 #Read input
 function input (){
@@ -26,12 +30,16 @@ read stage
 #FUNCTIONS
 #Start pt ununtu
 function ubuntustart(){
+	echo -e $Updatevar
+	sudo apt-get -y update;sudo apt-get -y upgrade
+	sudo apt-get -y -qq install wget 
+	echo -e $Envar
 	mkdir $arh
 	cp $confu $arh 2>/dev/null
 	rm $confu 2>/dev/null
-	sudo apt-get -y -qq install wget 
 	wget -nc -P /etc/apache2 'https://github.com/SebastianDorobantu/Linux/blob/master/apache2.conf'
 	mv /var/www/html/* $arh 2>/dev/null
+	echo -e $Compvar
 }
 
 
@@ -41,20 +49,32 @@ function ubuntustop(){
 	mv $arh/apache2.conf $confu
 	mv $arh/* /var/www/html/ 2>/dev/null
 	rm -r $arh
-
+	echo -e "$Restvar"
 }
 
 
 #Start on centos
-function ubuntustart(){
+function centosstart(){
+	echo -e $Updatevar
+	sudo yum -y update;sudo yum -y upgrade
+	sudo yum -q -y nstall wget
+	echo -e $Envar
 	mkdir $arh
 	cp $confc $arh 2>/dev/null
 	rm $confc 2>/dev/null
-	sudo apt-get -y -qq install wget 
 	wget -nc -P /etc/httpd/conf/ 'https://github.com/SebastianDorobantu/Linux/blob/master/httpd.conf'
 	mv /var/www/html/* $arh 2>/dev/null
+	echo -e $Compvar
 }
 
+#Stop pt centos
+function centosstop(){
+	rm $confc 2>/dev/null
+	mv $arh/httpd.conf $confc
+	mv $arh/* /var/www/html/ 2>/dev/null
+	rm -r $arh
+	echo -e $Restvar
+}
 
 
 
@@ -78,5 +98,10 @@ if [[ $os != '' ]]; then
 		ubuntustop
 	fi
 else 
-	echo $os
+	if [[ $stage -eq '1' ]]
+	then 
+		centosstart
+	else
+		centosstop
+	fi
 fi
